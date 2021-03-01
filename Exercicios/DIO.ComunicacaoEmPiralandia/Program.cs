0b9b721455a327b6e5c43b98dcf9cc1ef4130440
dayanFreitas <dayan.freitas@catolicasc.org.br>
2021-02-28 22:41:48 -0300

    Novo desafio adicionado

---
+++ Exercicios/DIO.ComunicacaoEmPiralandia/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace DIO.ComunicacaoEmPiralandia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            MatchCollection codigo = Regex.Matches(n, "[0-9]");
+            List<string> lstring = new List<string>();
+
+            for(int i=0;i < codigo.Count; i++) 
+            {
+                lstring.Add(codigo[i].Value);
+            }
+        
+            lstring.Reverse();
+            foreach (string el in lstring)
+            {
+                Console.Write($"{el}");
+            }
+        }
+    }
+}
